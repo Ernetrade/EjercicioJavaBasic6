a895b4e60fcfd444be3550cb1b466196b42797d1
legiónDev <ernetrades@gmail.com>
2023-01-13 01:46:50 -0500

    Finish

---
+++ src/CocheCRUDImp.java
@@ -0,0 +1,24 @@
+public class CocheCRUDImp implements CocheCRUD{
+
+    public CocheCRUDImp() {
+    }
+
+    @Override
+    public void save() {
+        System.out.println("save");
+        
+    }
+
+    @Override
+    public void findAll() {
+        System.out.println("findAll");
+        
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("delete");
+        
+    }
+    
+}
